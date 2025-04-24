bc0d628fb114bcf6f2eb6c69ac995fcd5c09c71f
Tubaarpacay <tubaarpacay@posta.mu.edu.tr>
2025-04-24 13:18:58 +0300

    Update Shape.java

--- abstract_shape/Shape.java
+++ abstract_shape/Shape.java
@@ -1,4 +1,21 @@
 package PACKAGE_NAME;
 
-public class Shape {
+public abstract class Shape {
+    private String color="white";
+    public Shape(){}
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public abstract double perimeter();
+    public abstract double area();
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                '}';
+    }
 }
